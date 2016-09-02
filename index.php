38e6135b04ff428b7c14f6c75331afbc06c44da0
harelimana <alexandre.harelimana@gmail.com>
2016-09-02 08:35:28 +0200

    new file:   articles.php 	new file:   index.php 	css/ 	fonts/ 	js/ 	nbproject/

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Scripts clients - Seconde session 2016</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div>
+            <h2>Articles</h2> 
+            <button id="lire" type="button" class="btn btn-primary">Lire article</button>
+            <table id="articles" class="table">
+                <tr>
+                    <td>Marque</td>
+                    <td>Nom</td>
+                    <td>Prix</td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <h2>Commande</h2>
+            <table id="commande" class="table">
+                <tr>
+                    <td>Marque</td>
+                    <td>Nom</td>
+                    <td>Prix</td>
+                    <td>Quantite</td>
+                    <td>Sous_total</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+    <script src="js/libs/jquery/jquery.js" type="text/javascript"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <script src="js/main.js" type="text/javascript"></script>
+</html>
